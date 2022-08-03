7eb90edd16ad6fed1f6726fb64dbc196c7b1862b
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-03 14:18:58 -0400

    added users model and create user route

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,3 +1,5 @@
+const Users = require('../../models/Users');
+const bcrypt = require('bcrypt');
 const router = require('express').Router();
 
 router.post('/timeline',  (req, res) => {
@@ -11,6 +13,18 @@ router.post('/group', (req,res) => {
 router.post('/friend-request', (req,res) => {
 
 })
+
+router.post('./user', async (req,res) => {
+    try {
+        const newUser = req.body;
+        newUser.password = await bcrypt.has(req.body.password,10);
+        const userData = await Users.create(newUser);
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+}) 
 
 
 

4cdb70eeaf1885d16d004869f96f32d5a2befdff
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-03 13:46:00 -0400

    added more sample routes

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -4,4 +4,14 @@ router.post('/timeline',  (req, res) => {
     console.log('logic')
 })
 
+router.post('/group', (req,res) => {
+
+})
+
+router.post('/friend-request', (req,res) => {
+
+})
+
+
+
 module.exports = router;

1cb647ad46c20d6c52fbcaae52eefce4b426992d
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-03 11:08:25 -0400

    added connection and setup server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require("./controllers/routes")
+const sequelize = require('./config/connection');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');

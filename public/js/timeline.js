ea9674ed1b0e8deb1a8b57e8d1def416c7d368ad
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-04 14:04:56 -0400

    fixed bug with clicking on posts in timeline

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -25,7 +25,7 @@ const data = document.querySelectorAll('.timeline-post')
 console.log(data)
 
 data.forEach(element => {
-    addEventListener("click", getPost)
+    element.addEventListener("click", getPost)
     console.log(element)
 })
 

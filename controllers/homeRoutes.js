9146923dfdb07512b7520bd394a05d7e0fe3aeb1
KRivera2394 <keanurivera2394@gmail.com>
2022-08-04 13:53:32 -0400

    Added Profile Box

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,4 @@
 const {Posts} = require('../models')
-const {Comment} = require('../models')
 const router = require('express').Router();
 const withAuth = require('../middleware/auth');
 const isLoggedIn = require('../middleware/loggedIn');
@@ -28,7 +27,7 @@ router.get('/signup', isLoggedIn, async (req,res) => {
     }
 })
 
-router.get('/home', withAuth,  async (req,res) => {
+router.get('/home',  async (req,res) => {
     try {
         const dbTimelineData = await Posts.findAll({
         });
@@ -37,75 +36,21 @@ router.get('/home', withAuth,  async (req,res) => {
         result.get({ plain: true })
     );
 
-        const dataid = []
         const posts = []
         for(let i =0; i<10; i++) {
            let post =  array[Math.floor(Math.random() * array.length)]
-           dataid.push(post.id)
             posts.push(post)
         }
-        console.log(dataid)
+
         console.log({posts})
         res.render('home', 
-          {posts, dataid},
+          {posts},
         );
       } catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
     });
-
-router.get('/post/:id', async (req,res) => {
-    try {
-        const dbTimelineData = await Posts.findAll({
-        });
-
-            const dbCommentData = await Comment.findAll({
-        where: {
-            on_post: req.params.id
-          },
-    });
-
-    const   comments = dbCommentData.map((result) =>
-    result.get({ plain: true })
-);
-
-
-        const array = dbTimelineData.map((result) =>
-        result.get({ plain: true })
-    );
-        const { posted_by, post_content } = array[req.params.id - 1]
-        req.session.save(()=> {
-            req.session.post_id = req.params.id
-        })
-
-        res.render('post',{posted_by, post_content,comments} );
-    } catch (err) {
-        console.log(err)
-        res.status(400).json(err);
-    }
-
-})
-
-router.get('/home/:username', async (req,res) => {
-    const username = req.params.username
-    const dbUserData = await Posts.findAll({
-      where: {
-        posted_by: req.params.username
-      }
-    });
-
-  
-    const posts = dbUserData.map((result) =>
-    result.get({ plain: true })
-  );
-    
-  
-    res.render('user', 
-    {posts, username},
-    );
-
-  })
 
 
 

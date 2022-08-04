5eeeb959b93f752f403a9945640b917e7c06d377
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-03 20:38:17 -0400

    added ids and routes from homepage

---
+++ public/js/routes.js
@@ -0,0 +1,10 @@
+document.getElementById('login').addEventListener('click', function() {
+    window.location = '/login'
+})
+console.log('linked')
+
+document.getElementById('signup').addEventListener('click', function() {
+    window.location = '/signup'
+})
+
+console.log(window.location.href)
